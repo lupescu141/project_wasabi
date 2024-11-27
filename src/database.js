92fbdeb4f7fec15679492972388b2ed13c00d483
Daniel Lupescu <d.lupescu@hotmail.fi>
2024-11-27 20:22:24 +0200

    added .env

--- src/database.js
+++ src/database.js
@@ -1,11 +1,13 @@
 import mysql from "mysql2";
+import dotenv from "dotenv";
+dotenv.config();
 
 const pool = mysql
   .createPool({
-    host: "127.0.0.1",
-    user: "root",
-    password: "Mahry4!11?",
-    database: "wasabi",
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
   })
   .promise();
 
